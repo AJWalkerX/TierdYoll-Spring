56a9b5ad4274902c243a98dc616887fba4c2d108
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-10-21 14:31:17 +0300

    first commit

---
+++ src/main/java/com/ajwalker/tierdyol/TierdYolApplication.java
@@ -0,0 +1,13 @@
+package com.ajwalker.tierdyol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierdYolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TierdYolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajwalker/tierdyol/TierdYolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajwalker.tierdyol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierdYolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
