ded558fec715faf9503bfd8be4eac8cada912351
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-10-25 15:58:07 +0300

    Order Bug fixed!

--- src/main/java/com/ajwalker/service/OrderService.java
+++ src/main/java/com/ajwalker/service/OrderService.java
@@ -3,11 +3,13 @@ package com.ajwalker.service;
 import com.ajwalker.dto.request.OrderByBasketRequestDto;
 import com.ajwalker.entity.Basket;
 import com.ajwalker.entity.Order;
+import com.ajwalker.entity.User;
 import com.ajwalker.exception.ErrorType;
 import com.ajwalker.exception.TierdYolException;
 import com.ajwalker.repository.OrderRepository;
 import com.ajwalker.utility.enums.EBasketState;
 import com.ajwalker.utility.enums.EOrderState;
+import com.ajwalker.utility.enums.EUserStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +20,17 @@ import java.util.Optional;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final BasketService basketService;
+    private final UserService userService;
 
     public void orderByBasket(OrderByBasketRequestDto dto) {
+        Optional<Basket> optionalBasket = basketService.findByBasketId(dto.basketId());
+        if (optionalBasket.isEmpty()) {
+            throw new TierdYolException(ErrorType.NOT_FOUND_BASKET);
+        }
+        Optional<User> optionalUser = userService.findById(dto.userId());
+        if (optionalUser.isEmpty() || optionalUser.get().getUserStatus() != EUserStatus.USER) {
+            throw new TierdYolException(ErrorType.INVALID_OR_NOTFOUND_USER);
+        }
         Order order = Order.builder()
                 .userId(dto.userId())
                 .basketId(dto.basketId())
@@ -27,11 +38,7 @@ public class OrderService {
                 .orderDate(System.currentTimeMillis())
                 .build();
         orderRepository.save(order);
-
-        Optional<Basket> optionalBasketId = basketService.findByBasketId(dto.basketId());
-        if (optionalBasketId.isPresent()) {
-            basketService.updateBasketStateById(dto.basketId(),EBasketState.PASSIVE);
-        }
+        basketService.updateBasketStateById(dto.basketId(), EBasketState.PASSIVE);
 
     }
 
